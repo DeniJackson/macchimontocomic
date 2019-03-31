49a0571e2b5bd89e9bf2b3f114532d83019fe932
DJ <konobeat@gmail.com>
2019-03-31 10:44:53 -0400

    Updates

---
+++ precache-manifest.252feaf9b035744fadeb1a6b27fa8fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maccimonto/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c28039250c5072989c2",
+    "url": "/maccimonto/static/js/runtime~main.28bc998d.js"
+  },
+  {
+    "revision": "a2ea8d67d76684451567",
+    "url": "/maccimonto/static/js/main.0a9f4556.chunk.js"
+  },
+  {
+    "revision": "412828b853ec22c56b5c",
+    "url": "/maccimonto/static/js/2.fccc110c.chunk.js"
+  },
+  {
+    "revision": "a2ea8d67d76684451567",
+    "url": "/maccimonto/static/css/main.1290a609.chunk.css"
+  },
+  {
+    "revision": "412828b853ec22c56b5c",
+    "url": "/maccimonto/static/css/2.767c95cf.chunk.css"
+  },
+  {
+    "revision": "0a6ba246117311693940672d1d9870e4",
+    "url": "/maccimonto/index.html"
+  }
+];
